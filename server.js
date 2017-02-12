48de1d779e3162cb9bf09d3988f9f5d32b2eb218
Pedro Correa <pedroorez@gmail.com>
2017-02-12 13:01:12 -0200

    Adds save and response to /create.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require('express')();
+const alias = require('./alias');
 const mongoose = require('mongoose');
 const isURLvalid = require('valid-url').isWebUri;
 
@@ -29,6 +30,27 @@ app.get('/create', (req, res) => {
   if (!isURLvalid(req.query.url)) {
     return res.send('URL Non valido');
   }
+  const urlAlias = alias.gen();
+
+  return Shorturl
+    .find({ alias: urlAlias })
+    .then((search) => {
+      if (search.length > 0) {
+        res.send('Already Exist');
+      }
+      return Shorturl
+        .create({
+          url: req.query.url,
+          alias: urlAlias,
+        })
+        .then((result) => {
+          res.send({
+            alias: result.alias,
+            url: `${req.headers.host}/${result.alias}`,
+          });
+        })
+    });
+});
 });
 
 // Listen Port
