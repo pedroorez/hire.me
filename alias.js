502eaebb77cf620378ca7ec7641e4495947a1129
Pedro Correa <pedroorez@gmail.com>
2017-02-12 12:54:09 -0200

    Adds Alias gen/check module.

---
+++ alias.js
@@ -0,0 +1,12 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
+const alphabetLength = alphabet.length;
+
+module.exports.gen = () => {
+  let alias = []; // eslint-disable-line
+  for (let i = 0; i < 6; i += 1) {
+    alias[i] = alphabet[Math.floor(Math.random() * alphabetLength)];
+  }
+  return alias.join('');
+};
+
+module.exports.check = alias => !alias.split('').some(v => alphabet.indexOf(v) < 0);
